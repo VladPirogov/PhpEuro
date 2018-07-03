41e5a40668a026635a3d0e39d3193b5fe34f7258
Vlad Pirogov <pirogov1818@gmail.com>
2018-07-03 16:16:01 +0300

    successful connection to the database

--- connection.php
+++ connection.php
@@ -7,15 +7,15 @@
  */
 
  // З'єдняння з базою даних
- mysqli_connect("localhost", "root", "root") or die(mysqli_error());
- mysqli_select_db("Euro-App") or die(mysqli_error());
+$linc= mysqli_connect("localhost", "root", "root") or die(mysqli_error());
+$conn= mysqli_select_db($linc,"Euro-App") or die(mysqli_error());
 
 echo 'Подключение к базе данных.<br>';
 
-$result = mysqli_query('SELECT Name FROM User');
+$result = mysqli_query($linc,"SELECT Name FROM User");
 
 $row = mysqli_fetch_row($result);
 echo "Сотрудник 1: ", $row[0], "<br>\n";
 
-mysqli_close($conn);
+mysqli_close($linc);
 ?>
